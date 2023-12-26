c6f0c65ffd730a8f4a3af465991e7234609d8dfa
Muhammad Sheharyar Butt <56956294+shehari007@users.noreply.github.com>
2023-12-26 19:48:28 +0300

    added blink light

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var five = require("johnny-five")
-var cpuStat = require('cpu-stat')
 var si = require('systeminformation')
-const windowsCPU = require('windows-cpu');
-const os = require('os');
 var board = new five.Board()
 
 board.on("ready", function () {
@@ -10,32 +7,22 @@ board.on("ready", function () {
   var lcd = new five.LCD({
     controller: "PCF8574T"
   });
+  var led = new five.Led(13);
+  
+  lcd.backlight(50);
 
   setInterval(() => {
-
-    windowsCPU.totalLoad()
-    .then(load => {
-      const currentCPULoad = load; // multiplying by 100 for percentage value
-      // console.log('CPU Load:', currentCPULoad + '%');
-      // Display the current CPU load value on your LCD or perform other actions
-      lcd.cursor(0, 0).print('CPU:');
-      lcd.cursor(0, 4).print(parseInt(currentCPULoad) + '%');
-    })
-    .catch(error => {
-      //console.error('Error fetching CPU load:', error);
-      lcd.cursor(0, 0).print('CPU: ERR');
-    });
-    // si.currentLoad()
-    //   .then(data => {
-    //     // console.log('Current CPU Load:', data);
-    //     // console.log('Current CPU Load (in percentage):', data.currentLoad);
-    //     lcd.cursor(0, 0).print('CPU:');
-    //     lcd.cursor(0, 4).print(parseInt(data.currentLoad)+'%');
-
-    //   })
-    //   .catch(error => {
-    //     lcd.cursor(0, 0).print('CPU: ERR');
-    //   });
+    si.currentLoad()
+      .then(data => {
+        // console.log('Current CPU Load:', data);
+        // console.log('Current CPU Load (in percentage):', data.currentLoad);
+        lcd.cursor(0, 0).print('CPU:');
+        lcd.cursor(0, 4).print(parseInt(data.currentLoad)+'%');
+        parseInt(data.currentLoad)>85?led.blink(500): led.stop().off();
+      })
+      .catch(error => {
+        lcd.cursor(0, 0).print('CPU: ERR');
+      });
     si.mem(function (data) {
       lcd.cursor(0, 9).print('MEM:' + parseInt(((1 - (data.available / data.total)) * 100)) + '%');
     })
@@ -49,5 +36,5 @@ board.on("ready", function () {
     })
     lcd.cursor(0, 4)
     lcd.print('     ')
-  }, 1000)
+  }, 1500)
 });
